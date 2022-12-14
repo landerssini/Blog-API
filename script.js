e85d06fcbca7905e866e6e3e7ec54bc5eacff5ca
Lander Sola <landersolaaguirre@gmail.com>
2022-12-14 18:50:24 +0100

    Mejorado todo

--- script.js
+++ script.js
@@ -1,52 +1,105 @@
 let postsDisplay = document.querySelector("#postsDisplay")
 let buttonModal = document.querySelectorAll(".btnModal")
+let username = document.querySelector("#username")
+let email = document.querySelector("#email")
+let modalTitle = document.querySelector("#modalTitle")
+let postBody = document.querySelector(".modal-body")
+let commentsDisplay= document.querySelector("#commentsDisplay")
+let commentNum= document.querySelector("#commentNum")
+let commentBtn
 let postId
-var respuesta
-var datos
-const cargarPosts = async () => {
-  respuesta = await fetch("db.json")
-  console.log(respuesta)
-  datos = await respuesta.json()
-  console.log(datos);
-  datos.posts.forEach(post => {
+let datosPosts
+let datosUsers
+let datosUser
+let datosPost
+let datosComments
+let datosCommentsPost
+let postUserId
+let b
+
+function refresh() {
+  fetch("http://localhost:3000/posts")
+    .then(Response => Response.json())
+    .then(data => datosPosts = data)
+    .then(cargarPosts)
+    .then(assignBtn)
+  fetch("http://localhost:3000/users")
+    .then(Response => Response.json())
+    .then(data => datosUsers = data)
+  fetch("http://localhost:3000/comments")
+    .then(Response => Response.json())
+    .then(data => datosComments = data)
+}
+refresh()
+function cargarPosts() {
+  datosPosts.forEach(post => {
     if (post.id < 22) {
       postsDisplay.innerHTML +=
         `<div class="card">
-          <div class="card-body">
-            <h5 class="card-title">${post.title}</h5>
-            <button type="button" class="btn btn-primary btnModal" onclick="showModal()" data-bs-toggle="modal"  data-bs-target="#modal${post.id}">
-              Open
-            </button>
-          </div>
-        </div>
-        <div class="modal fade" id="modal${post.id}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">${post.title}</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-      ${post.body}
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <div class="dropdown">
-        <div class="dropdown">
-        <button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-          Actions
-        </button>
-        <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-          <li><a class="dropdown-item list-group-item-action list-group-item-warning" id="editpost${post.id}" href="#">Edit post</a></li>
-          <li><a class="dropdown-item list-group-item-action list-group-item-danger" id="deletepost${post.id}" href="#">Delete post</a></li>
-        </ul>
-      </div>
-</div>
-      </div>
-    </div>
-  </div>
-</div>`
+              <div class="card-body">
+                <h5 class="card-title">${post.title}</h5>
+                <button type="button" class="btn btn-primary btnModal" id="${post.id}" user="${post.userId}"data-bs-toggle="modal"  data-bs-target="#modal">
+                  Open
+                </button>
+                <h3></h3>
+              </div>
+            </div>`
     }
-  });
+  })
 }
-cargarPosts()
+function assignBtn() {
+  buttonModal = document.querySelectorAll(".btnModal")
+  buttonModal.forEach(btn => {
+    btn.addEventListener("click", showModal)
+  })
+}
+function getUser(i) {
+  fetch(`http://localhost:3000/users/${i}`)
+    .then(Response => Response.json())
+    .then(data => datosUser = data)
+    .then(datosUser => username.textContent = datosUser.username)
+    .then(data => email.textContent = datosUser.email)
+}
+function getPost(i) {
+  fetch(`http://localhost:3000/posts/${i}`)
+    .then(Response => Response.json())
+    .then(data => datosPost = data)
+    .then(datosPost => postBody.innerHTML = datosPost.body)
+    .then(data => modalTitle.innerHTML = datosPost.title)
+}
+function getComments(i) {
+  datosCommentsPost = null
+  datosCommentsPost = datosComments.filter(comment => comment.postId == postId)
+  commentBtn = document.querySelector("#commentBtn")
+  commentBtn.addEventListener("click",showComments)
+  
+}
+function assignBtnComment() {
+  commentBtn = document.querySelector("#commentBtn")
+  commentBtn.addEventListener("click",showComments)
+  b=0
+  
+}
+function showComments(){
+  commentsDisplay.innerHTML=" "
+  datosCommentsPost.forEach(commentpost=> commentsDisplay.innerHTML +=`<div class="commentBox"><h1 class="commentTitle">${commentpost.name}</h1><br><p class="commentBody">${commentpost.body}</p></div><hr>`)
+  if (b==0){
+    commentsDisplay.style.opacity="100%"
+    b+=1
+  }
+  else if (b==1){
+    commentsDisplay.style.opacity="0%"
+    b-=1
+  }
+}
+function showModal(e) {
+  b=0
+  postUserId = e.path[0].attributes.user.value
+  postId = e.path[0].attributes.id.value
+  getPost(postId)
+  getUser(postUserId)
+  getComments(postId)
+  showComments()
+  assignBtnComment()
+  commentsDisplay.style.opacity="0%"
+} 
